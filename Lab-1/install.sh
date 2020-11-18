0b7ce8548f02621ac3cc896cbf4d48919b31d211
root <gurret.lionel@gmail.com>
2020-11-18 13:23:05 +0000

    New version of script

--- Lab-1/install.sh
+++ Lab-1/install.sh
@@ -3,6 +3,9 @@
 # Lionel Gurret
 # 18th Nov 2020
 
+#url: PROMETHEUS_URL in grafana.yml
+sed -i 's,PROMETHEUS_URL,http://google.com,' grafana.yml
+
 # Ask for Minikube public URL (ex: https://2886795331-cykoria04.environments.katacoda.com/)
 echo "Please provie Minikube public URL (ex: https://2886795331-cykoria04.environments.katacoda.com/)"
 read -p "You can get it by clicking on + / Select port to view on Host 1: " answer
@@ -38,17 +41,25 @@ helm repo add grafana https://grafana.github.io/helm-charts
 # Create a namespace for our stacs
 kubectl create ns metrics
 
-# Install (or upgrade) Prometheus / Grafana Stack (adapt file if you use custom files)
+# Install (or upgrade) Prometheus (adapt yml file if you use custom files)
 helm upgrade --install prometheus -f prometheus.yml -n metrics prometheus-community/prometheus
-helm upgrade --install grafana -f grafana.yml -n metrics grafana/grafana
-
-# Expose services to reach them externally
+# Expose service to reach them externally
 kubectl expose service prometheus-server --type=NodePort --target-port=9090 --name=prometheus-server-np -n metrics
+
+# Get Prometheus Endpoint
+PORT_PROM=$(kubectl -n metrics get service prometheus-server-np -o yaml|grep nodePort|awk -F ': ' {'print $2'})
+
+# With Prometheus port, we can configure grafana.yml
+$PROMETHEUS_URL=$first-$PORT_PROM-$second
+sed -i 's,PROMETHEUS_URL,$PROMETHEUS_URL,' grafana.yml
+
+# Install (or upgrade) Grafana  (adapt yml file if you use custom files)
+helm upgrade --install grafana -f grafana.yml -n metrics grafana/grafana
+# Expose service to reach them externally
 kubectl expose service grafana --type=NodePort --target-port=3000 --name=grafana-np -n metrics
 
-# Get Grafana / Prometheus Endpoint
+# Get Grafana Endpoint
 PORT_GRAF=$(kubectl -n metrics get service grafana-np -o yaml|grep nodePort|awk -F ': ' {'print $2'})
-PORT_PROM=$(kubectl -n metrics get service prometheus-server-np -o yaml|grep nodePort|awk -F ': ' {'print $2'})
 
 # Replace Prometheus URL in grafana.yml config file
 echo "URL PROMETHEUS: $first-$PORT_PROM-$second"
@@ -58,7 +69,7 @@ echo "**************************************************************************
 echo "********************************** ENVIRONMENT CONFIGURED YOU CAN PLAY NOW **********************************"
 echo "*************************************************************************************************************"
 
-echo "You can logon Prometheus Web Interface through this port : $PORT_PROM"
+echo "You can logon Prometheus Web Interface through this url : $PROMETHEUS_URL"
 
 # kubectl -n metrics get services -o yaml|grep -i nodePort:
 # Display secret to use 
