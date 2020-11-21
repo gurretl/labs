e6d904376366fda449c410860e2c2723cd77984e
gurretl <73423955+gurretl@users.noreply.github.com>
2020-11-21 14:27:32 +0100

    Update config_devenv.sh

---
+++ Lab-1/config_devenv.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "[1] : change keyboard layout to Swiss"
+sudo sed -i 's/XKBLAYOUT=\"\w*"/XKBLAYOUT=\"ch\"/g' /etc/default/keyboard
+
+echo "[2]: Install docker if not exist"
+if [ ! -f "/usr/bin/docker" ];then
+curl -s -fsSL https://get.docker.com | sh; 
+fi
+
+echo "[3]: Install GUI"
+pkg="ubuntu-desktop"
+if dpkg -s $pkg
+then
+    echo "$pkg installed"
+else
+    echo "$pkg not installed"
+    sudo apt-get update -y && sudo apt-get upgrade -y
+	sudo apt-get install ubuntu-desktop -y
+	sudo reboot
+fi
+
+echo "[4]: Install packages"
+sudo apt-get install mc net-tools -y
