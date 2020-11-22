a8dfce3e8f482fbab85a68346caa8c183794fdce
Gurret Lionel <gurret.lionel@gmail.com>
2020-11-22 16:14:40 +0000

    Last fixes

--- Lab-2/3-install-helloworld-app.sh
+++ Lab-2/3-install-helloworld-app.sh
@@ -1,7 +1,6 @@
-!#/bin/bash
 echo "[1] Git Clone helloworld app from repo"
 ssh vagrant@192.168.56.12 'sudo yum install git -y'
-ssh vagrant@192.168.56.12 'git-force-clone https://github.com/skynet86/hello-world-k8s.git'
+ssh vagrant@192.168.56.12 'git clone https://github.com/skynet86/hello-world-k8s.git'
 echo "[2] Install app with kubectl command"
 ssh vagrant@192.168.56.12 'kubectl create -f /home/vagrant/hello-world-k8s/hello-world.yaml'
 echo "[3] Open your browser at the following address :"
