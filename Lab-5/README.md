61ee0f4d2592b34a62f35b5f6283e4678b032e70
Gurret Lionel <gurret.lionel@gmail.com>
2020-11-28 14:56:15 +0000

    New Lab

---
+++ Lab-5/README.md
@@ -0,0 +1,12 @@
+# About
+Date : 28 Nov 2020  
+Author: Lionel Gurret
+# LinkedIn article related
+# Prerequisites
+Get a Docker environment.  
+I.e : You can use something like : https://kubernetes.io/fr/docs/tutorials/hello-minikube/ (Launch Terminal)
+# How to launch the Lab
+git clone https://github.com/labs
+cd labs/Lab-5
+./run.sh
+
---
+++ Lab-5/run.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Lionel Gurret
+# 28 Nov 2020
+echo "[1] Let's build our first image !"
+sleep 2
+docker image build -t gurretl/myfirstimage:v1 .
+sleep 2
+
+echo "[2] Our image is built, let's display it :"
+sleep 2
+docker image ls
+sleep 5
+
+echo "[3] List image layers : "
+sleep 2
+docker image history gurretl/myfirstimage:v1
+sleep 5
+
+echo "[4] Let's a run container for testing"
+sleep 2
+docker container run -itd --name=testimage -p 8080:80 gurretl/myfirstimage:v1
+
+echo "[5] Testing :"
+sleep 2
+docker container ls -l
+sleep 3
+curl http://localhost:8080 -s
+
+
