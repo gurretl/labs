bf1468a033a3cf08c834522f32bf0ce18faa1b41
Gurret Lionel <gurret.lionel@gmail.com>
2020-12-08 13:13:08 +0000

    Improve run.sh

--- Lab-6/run.sh
+++ Lab-6/run.sh
@@ -1,33 +1,26 @@
 #!/bin/bash
-
 # Lionel Gurret
-# 28 Nov 2020
-echo "[1] Let's run a container with an health check !"
+# 08 Dec 2020
+echo "[1] Pull Image ngixn"
+docker pull nginx:1.13
+echo "[2] Let's run a container with an health check !"
 sleep 2
 docker run --name=nginx-proxy -d --health-cmd='stat /etc/nginx/nginx.conf || exit 1' nginx:1.13
-while [ $(/usr/bin/docker inspect -f {{.State.Health.Status}} nginx-proxy) == "starting" ]
-do
-  echo "In until"
-  sleep 1;
-done
-echo "Out until"
+while [ "`docker inspect -f {{.State.Health.Status}} nginx-proxy`" != "healthy" ]; do     sleep 2; done
+#sleep 35
 /usr/bin/docker inspect -f {{.State.Health.Status}} nginx-proxy
-echo "AA"
-exit
-
-echo "[2] Is our container Healthy ?"
+echo "[3] Is our container Healthy ?"
 sleep 2
 /usr/bin/docker inspect -f {{.State.Health.Status}} nginx-proxy
 sleep 5
-
-echo "[3] Let's make our container Unhealthy (remove nginx.conf)"
+echo "[4] Let's make our container Unhealthy (remove nginx.conf)"
 sleep 2
 docker exec nginx-proxy rm /etc/nginx/nginx.conf
 sleep 5
-
-echo "[4] Is our container Healhty ?"
+echo "[5] Is our container Healhty ?"
 sleep 2
 /usr/bin/docker inspect -f {{.State.Health.Status}} nginx-proxy
+exit
 sleep 3
 echo "[6] Fixing issue (create nginx.conf)"
 sleep 2
