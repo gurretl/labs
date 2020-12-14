742b6c4b855e7d46c9fadf031fdce44c3a3c43f4
Gurret Lionel <73423955+gurretl@users.noreply.github.com>
2020-12-14 16:10:11 +0100

    Update run.sh

--- Lab-8/run.sh
+++ Lab-8/run.sh
@@ -25,8 +25,12 @@ echo "[3] Install K9S"
 (cd && tar -xzf k9s_Linux_x86_64.tar.gz)
 (cd && mv k9s /usr/local/bin/k9s)
 sleep 2
-echo "[4] Get Jenkins password"
+echo "[4] Install Maven on Pod"
 JENKINS_POD=$(kubectl get pods -n jenkins|grep jenkins|awk -F ' ' {'print $1'})
+kubectl -n jenkins exec $JENKINS_POD -- apt update -y
+kubectl -n jenkins exec $JENKINS_POD -- apt install maven -y
+sleep 2
+echo "[5] Get Jenkins password"
 JENKINS_PWD=$(kubectl logs $JENKINS_POD --namespace jenkins|grep "Please use " -A3|grep -v "Please"|grep -v '^$')
 sleep 2
 echo "*************************************************************************************************************"
