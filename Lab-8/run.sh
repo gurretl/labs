bdc3f2ea2319464e017fd6b6e67d6af1c35bb988
Gurret Lionel <gurret.lionel@gmail.com>
2020-12-14 11:48:48 +0000

    Add PVs

--- Lab-8/run.sh
+++ Lab-8/run.sh
@@ -7,6 +7,8 @@ echo "Please provie Minikube public URL (ex: https://2886795331-cykoria04.enviro
 read -p "You can get it by clicking on + / Select port to view on Host 1: " answer
 echo "Answer : $answer"
 
+mkdir /mnt/data
+
 url_first=$(echo $answer|awk -F '-' {'print $1'})
 url_second=$(echo $answer|awk -F '-' {'print $2'})
 
@@ -16,7 +18,7 @@ kubectl create ns jenkins
 echo "[2] Deploy Jenkins"
 sleep 2
 kubectl apply -f .
-sleep 15
+sleep 20
 echo "[3] Get Jenkins password"
 JENKINS_POD=$(kubectl get pods -n jenkins|grep jenkins|awk -F ' ' {'print $1'})
 JENKINS_PWD=$(kubectl logs $JENKINS_POD --namespace jenkins|grep "Please use " -A3|grep -v "Please"|grep -v '^$')
