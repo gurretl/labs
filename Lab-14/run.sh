aa18102c59fb4cb34fe6b5c9ea9a7598d9a5019f
Gurret Lionel <gurret.lionel@gmail.com>
2021-01-09 09:15:11 +0000

    Improve run.sh

--- Lab-14/run.sh
+++ Lab-14/run.sh
@@ -10,20 +10,23 @@ echo "[2] Install K9S"
 (cd && tar -xzf k9s_Linux_x86_64.tar.gz)
 (cd && mv k9s /usr/local/bin/k9s)
 sleep 2
-while [ "$(kubectl get pod emptydir -o jsonpath='{.items[*].status.containerStatuses[0].ready}')" != "true" ]; do
+while [ "$(kubectl get pod emptydir -o jsonpath='{.status.phase}')" != "Running" ]; do
    sleep 5
    echo "Waiting for Pod to be ready."
 done
 echo "[3] Copy a dummy file on first container"
-echo ""
+echo "Running : kubectl cp dummy.txt emptydir:/tmp/container-1-dir -c container-1"
 kubectl cp dummy.txt emptydir:/tmp/container-1-dir -c container-1
 sleep 2
 echo "[4] Check that the file has been copied"
-echo ""
+echo "Running : kubectl exec emptydir -c container-1 -- ls /tmp/container-1-dir"
 kubectl exec emptydir -c container-1 -- ls /tmp/container-1-dir
 sleep 2
 echo "[5] Check that the file is available on second container"
-echo ""
+echo "Running : kubectl exec emptydir -c container-2 -- ls /tmp/container-2-dir"
 kubectl exec emptydir -c container-2 -- ls /tmp/container-2-dir
 sleep 2
+echo "[6] It looks fine, what's inside that file ? :)"
+echo "Running: kubectl exec emptydir -c container-1 -- cat /tmp/container-1-dir/dummy.txt"
+kubectl exec emptydir -c container-1 -- cat /tmp/container-1-dir/dummy.txt
 echo "That's it ! You can run k9s command to check what have been done."
