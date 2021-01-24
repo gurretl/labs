98f16cff63c0d1ec65b6b5a6ea5f31e3f7cb36bd
Gurret Lionel <gurret.lionel@gmail.com>
2021-01-24 14:46:30 +0000

    Add Lab-15

---
+++ Lab-15/README.md
@@ -0,0 +1,13 @@
+# About
+Date : 24 Jan 20201
+Author: Lionel Gurret  
+Description : Build an Ubuntu Apache image on Docker !
+# LinkedIn article related
+# Prerequisites
+Get a Docker environment.  
+I.e : You can use something like : https://kubernetes.io/fr/docs/tutorials/hello-minikube/ (Launch Terminal)
+# How to launch the Lab
+`git clone https://github.com/labs`  
+`cd labs/Lab-15`  
+`./run.sh`
+
---
+++ Lab-15/run.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Lionel Gurret
+# 28 Nov 2020
+echo "[1] Let's build our first image !"
+sleep 2
+docker image build -t gurretl/myubuntuapache:v1 .
+sleep 2
+
+echo "[2] Our image is built, let's display it :"
+sleep 2
+docker image ls|grep gurretl
+sleep 5
+
+echo "[3] List image layers : "
+sleep 2
+docker image history gurretl/myubuntuapache:v1
+sleep 5
+
+echo "[4] Let's a run container for testing"
+sleep 2
+docker container run -itd --name=testubuntuimage -p 8080:8087 gurretl/myubuntuapache:v1
+
+echo "[5] Testing :"
+sleep 2
+docker container ls -l
+sleep 3
+curl http://localhost:8080 -s
+
+
